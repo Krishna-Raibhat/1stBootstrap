48fba3dc2f531a53f7e0c55104ca00921ece5a72
Krishna-Raibhat <krishnarb08@gmail.com>
2024-08-18 12:31:30 +0545

    comment

---
+++ assests/js/app.js
@@ -0,0 +1,12 @@
+document.getElementById("demo").innerHTML='Hello World';
+
+var x=document.getElementsByClassName('new');
+document.getElementById('new').innerHTML=
+    x[0].innerHTML + x[1].innerHTML + x[2].innerHTML;
+
+document.getElementsByTagName("h1")[0].innerHTML = "hello world";
+
+document.querySelector("#demo").innerHTML = "Hello World";
+document.querySelector(".new").innerHTML = "Hello World"; 
+
+document.querySelector(".new").innerHTML = "Hello World";
